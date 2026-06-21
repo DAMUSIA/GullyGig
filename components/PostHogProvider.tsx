2951e48ef50464bc726af496f40cda36333e5b3d
indresh404 <indreshsuresh95@gmail.com>
2026-06-21 23:25:58 +0530

    fixed error

--- components/PostHogProvider.tsx
+++ components/PostHogProvider.tsx
@@ -2,7 +2,7 @@
 
 import posthog from "posthog-js";
 import { PostHogProvider as PHProvider } from "posthog-js/react";
-import { useEffect } from "react";
+import { useEffect, Suspense } from "react";
 import PostHogPageView from "./PostHogPageView";
 
 export default function PostHogProvider({
@@ -21,7 +21,9 @@ export default function PostHogProvider({
 
   return (
     <PHProvider client={posthog}>
-      <PostHogPageView />
+      <Suspense fallback={null}>
+        <PostHogPageView />
+      </Suspense>
       {children}
     </PHProvider>
   );
